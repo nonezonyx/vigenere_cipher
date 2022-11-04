6929ead1c3820b9cc7476c9bfeb3387329eaf52c
NONEZONYX <nonezonyx@gmail.com>
2022-11-04 16:31:29 +0300

    added decrypt mode

--- vigenere_cipher.py
+++ vigenere_cipher.py
@@ -3,16 +3,18 @@ alphabet_rus="фбвгдеёжзиклмнопрстуфхцчшщъыьэюя"
 
 alphabets=[alphabet_eng,alphabet_rus]
 
-def vigenere_convert(str: str = "None", rot: str = "abc") -> str:
-    """vigenere_convert(str: str = "None", rot: str = "abc") -> str:
-Attention: rot must be the same language as str"""
+def vigenere_convert(str: str = "None", rot: str = "abc", mode: int|bool = 0) -> str:
+    """vigenere_convert(str: str = "None", rot: str = "abc", mode: int|bool = 0) -> str:
+rot \t Key .
+mode\t o - encrypt, 1 - decrypt.
+Attention: rot must be the same language as str."""
     ans=""
     k=0
     for i,s in enumerate(str):
-        for alphabet in alphabets:
-            if s.lower() in alphabet:
-                letter=alphabet[(alphabet.index(s.lower())+alphabet.index(rot[k]))%len(alphabet)]
-                s=(letter.lower(),letter.upper())[s.isupper()]
+        for abc in alphabets:
+            if s.lower() in abc:
+                char=abc[(abc.index(s.lower())+abc.index(rot[k])*(1,-1)[mode])%len(abc)]
+                s=(char.lower(),char.upper())[s.isupper()]
                 k=(k+1)%len(rot)
         ans+=s
     return(ans)
@@ -21,4 +23,5 @@ if __name__ == '__main__':
     while True:
         rot=input("Enter shift (key): ").lower()
         text=input("Enter text: ")
-        print(f"> {vigenere_convert(text,rot)}")
+        mode=input("Enter mode, encrypt or decrypt (e/d): ").lower()=="d"
+        print(f"> {vigenere_convert(text,rot,mode)}")
